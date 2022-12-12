ecb5d41742f7237813ddc09108c6cf47802dcb5e
iam-yuya <112651372+iam-yuya@users.noreply.github.com>
2022-12-12 07:30:43 +0000

    [Update]ルーティング追加,bootstrap導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,38 @@
 Rails.application.routes.draw do
+
+
+  # 会員側のルーティング
+  scope module: :public do
+    root to: "homes#top"
+    get "about" => "homes#about"
+
+    get "end_users" => "end_users#index", as: "end_users"
+    get "end_users/mypage" => "end_users#show", as: "mypage"
+    get "end_users/information/edit" => "end_users#edit", as: "edit_information"
+    patch "end_users/information" => "end_users#update", as: "update_information"
+    resources :posts, only: [:new, :index, :show, :create, :destroy, :update] do
+      resources :book_comments, only: [:create, :destroy]
+    end
+  end
+
+
+  # 管理者側のルーティング
+  namespace :admin do
+    get "top" => "homes#top"
+  end
+
+
+
+
+  # 会員用
   devise_for :end_users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+
+  # 管理者用
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
